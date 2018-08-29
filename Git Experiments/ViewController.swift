bfada297ed3b01737306c5ddd9d54755161176ab
Jack Manoog <jack@jacks-macbook-pro-2.local>
2018-08-29 01:18:40 -0400

    Initial Commit

---
+++ Git Experiments/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git Experiments
+//
+//  Created by Jack Manoog on 8/29/18.
+//  Copyright © 2018 Manoog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
